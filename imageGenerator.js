39f2671840b18a959b7ab694870d4f00f0b11ad9
Sam Jackson <sam@Sams-MacBook-Air.local>
2023-09-03 22:06:54 +0200

    image generator

---
+++ imageGenerator.js
@@ -0,0 +1,34 @@
+export function generateImage(canvas: any) {
+  const ctx = canvas.getContext("2d");
+
+  let imgWidth = 512
+  let imgHeight = 512
+
+  // You shoud push your rgba values to this imageDataArray
+  let imageDataArray : number[] = []
+
+  let imageData = ctx.createImageData(imgWidth, imgHeight)
+
+  // Your code goes here:
+
+  for (let i = 0; i < imgHeight; ++i) {
+      for (let j = 0; j < imgWidth; ++j) {
+          let r = Math.floor(255.999 * j / (imgWidth-1))
+          let g = Math.floor(255.999 * i / (imgHeight-1))
+          let b = Math.floor(255.999 * 0)
+
+          imageDataArray.push(r, g, b, 255)
+
+      }
+  }
+
+  for (let i = 0; i < imageData.data.length; i +=4) {
+          // Modify pixel data
+          imageData.data[i + 0] = imageDataArray[i + 0]; // R value
+          imageData.data[i + 1] = imageDataArray[i + 1]; // G value
+          imageData.data[i + 2] = imageDataArray[i + 2]; // B value
+          imageData.data[i + 3] = imageDataArray[i + 3]; // A value
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+}
